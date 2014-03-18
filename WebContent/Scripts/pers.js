31900804086ee746d8c1ffab3d97a749c94f0221
Helber <helber.neiva@gmail.com>
2014-03-18 13:39:41 -0300

    Teste

--- WebContent/Scripts/pers.js
+++ WebContent/Scripts/pers.js
@@ -3,7 +3,7 @@
  */
  ctx= document.getElementById("canvas").getContext("2d");
  var img = new Image();
- img.src = "Assets/img/robo.png";
+ img.src = "Assets/img/teste.png";
  img.onload= function(){
  	ctx.drawImage(img,0,0);
  };
